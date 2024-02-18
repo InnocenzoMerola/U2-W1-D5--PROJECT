dec4fd6ee3d6148b94b8a979215b7bb17acf2509
Innocenzo Merola <146184569+InnocenzoMerola@users.noreply.github.com>
2024-02-19 00:08:10 +0100

    upload nav scroll

--- project.js
+++ project.js
@@ -1,1 +1,15 @@
+let colorChange = document.querySelector(".nav-container");
+let button = document.querySelector(".btn");
+let height = 400;
 
+window.addEventListener("scroll", function () {
+  let scroll = window.scrollY;
+
+  if (scroll >= height) {
+    colorChange.style.backgroundColor = "white";
+    button.style.backgroundColor = "#1a8917";
+  } else {
+    colorChange.style.backgroundColor = "#ffc017";
+    button.style.backgroundColor = "#191919";
+  }
+});
